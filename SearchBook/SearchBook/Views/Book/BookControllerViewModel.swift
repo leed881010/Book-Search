4a8ce3ace8597259f24997e7ce44234b5fe2dd60
LeeD <lee.d.881010@gmail.com>
2022-02-19 23:32:54 +0900

    [Book] Set bookViewController UI & display datas

--- SearchBook/SearchBook/Views/Book/BookControllerViewModel.swift
+++ SearchBook/SearchBook/Views/Book/BookControllerViewModel.swift
@@ -10,6 +10,11 @@ import Foundation
 protocol BookControllerViewModelProtocol: AnyObject {
     
     func action(type: BookControllerViewModel.ActionType)
+    
+    var bookHandler: ((Book) -> Void)? { get }
+    func bind(book handler: @escaping(Book) -> Void)
+    
+    func fetchInitialData()
 }
 
 final class BookControllerViewModel {
@@ -18,8 +23,10 @@ final class BookControllerViewModel {
         self.initialData = initalData
     }
     
-    let initialData: InitialData
+    private let initialData: InitialData
+    private var itbookAPIConnector: ItBookAPIConnectorProtocol { NetworkDispatcher.shared.itbookAPIConnector }
     private(set) var popableHandler: ((BaseNavigationViewModel.Popable) -> Void)?
+    private(set) var bookHandler: ((Book) -> Void)?
 }
 
 extension BookControllerViewModel: BookControllerViewModelProtocol {
@@ -30,6 +37,20 @@ extension BookControllerViewModel: BookControllerViewModelProtocol {
         }
     }
     
+    func fetchInitialData() {
+        DispatchQueue.global(qos: .background).async {
+            self.itbookAPIConnector.books(request: BooksReqeust(isbn13: self.initialData.isbn13)) { [weak self] response, error in
+                if let error = error {
+                    print("error: \(error.message)")
+                } else if let response = response,
+                          let book: Book = .init(bookResponse: response) {
+                    self?.bookHandler?(book)
+                }
+            }
+        }
+    }
+    
+    func bind(book handler: @escaping (Book) -> Void) { self.bookHandler = handler }
 }
 
 extension BookControllerViewModel: BaseNavigationPopableProtocol {
--- SearchBook/SearchBook/Views/Book/BookViewController.swift
+++ SearchBook/SearchBook/Views/Book/BookViewController.swift
@@ -14,6 +14,7 @@ final class BookViewController: UIViewController {
         
         super.init(nibName: nil, bundle: nil)
         self.setupView()
+        self.bind()
     }
     
     required init?(coder: NSCoder) {
@@ -21,6 +22,107 @@ final class BookViewController: UIViewController {
     }
     
     private unowned let viewModel: BookControllerViewModelProtocol
+    private let stackView: UIStackView = {
+        let stackView: UIStackView = .init()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView: UIImageView = .init()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .boldSystemFont(ofSize: 30.0)
+        label.textColor = .black
+        return label
+    }()
+    private let subtitleLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: 15.0)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let authorsLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: 15.0)
+        label.textColor = .black
+        return label
+    }()
+    private let pagesLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: 15.0)
+        label.textColor = .black
+        return label
+    }()
+    private let priceLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: 15.0)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let yearLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: 15.0)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let ratingLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: 15.0)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let descLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: 15.0)
+        label.textColor = .black
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let urlLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: 15.0)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let isbn13Label: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: 15.0)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let publisherLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: 15.0)
+        label.textColor = .black
+        return label
+    }()
+    private let languageLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: 15.0)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let isbn10Label: UILabel = {
+        let label: UILabel = .init()
+        label.font = .systemFont(ofSize: 15.0)
+        label.textColor = .black
+        return label
+    }()
+    
 }
 
 private extension BookViewController {
@@ -32,9 +134,38 @@ private extension BookViewController {
 
 private extension BookViewController {
     
+    func bind() {
+        self.viewModel.bind(book: self.receive(book:))
+        self.viewModel.fetchInitialData()
+    }
+    
+    func receive(book: Book) {
+        DispatchQueue.main.async {
+            self.imageView.update(imageURL: book.imageURL, placeHolderImage: nil)
+            self.titleLabel.text = book.title
+            self.subtitleLabel.text = book.subtitle
+            self.pagesLabel.text =  "page: \(book.pages)"
+            self.ratingLabel.text =  "rating: \(book.rating)"
+            self.descLabel.text =  "desc: \(book.desc)"
+            self.urlLabel.text =  book.url.absoluteString
+            self.isbn13Label.text =  "isbn13: \(book.isbn13)"
+            self.priceLabel.text =  "price: \(book.price)"
+            self.yearLabel.text =  "year: \(book.year)"
+            self.publisherLabel.text =  "publisher: \(book.publisher)"
+            self.languageLabel.text =  "language: \(book.language)"
+            self.authorsLabel.text =  "authors: \(book.authors)"
+            self.isbn10Label.text =  "isbn10: \(book.isbn10)"
+             
+        }
+    }
+}
+
+private extension BookViewController {
+    
     func setupView() {
         self.view.backgroundColor = .white
         self.setupNavigationViewController()
+        self.setupContentView()
     }
     
     func setupNavigationViewController() {
@@ -47,4 +178,32 @@ private extension BookViewController {
         }()
         self.navigationItem.leftBarButtonItem = backBarButton
     }
+    
+    func setupContentView() {
+        [self.imageView, self.stackView, self.descLabel].forEach({
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview($0)
+        })
+        
+        [self.titleLabel, self.subtitleLabel, self.authorsLabel, self.priceLabel, self.ratingLabel, self.pagesLabel, self.urlLabel, self.isbn13Label, self.yearLabel, self.publisherLabel, self.languageLabel, self.isbn10Label]
+            .forEach { self.stackView.addArrangedSubview($0) }
+        
+        NSLayoutConstraint.activate([
+            self.imageView.widthAnchor.constraint(equalToConstant: 200.0),
+            self.imageView.heightAnchor.constraint(equalToConstant: 200.0),
+            self.imageView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.imageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            
+            self.stackView.topAnchor.constraint(equalTo: self.imageView.bottomAnchor),
+            self.stackView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.stackView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            
+            self.descLabel.topAnchor.constraint(equalTo: self.stackView.bottomAnchor),
+            self.descLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.descLabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            self.descLabel.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
 }
+
+
