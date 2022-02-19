250373ac27277193196d4f8d0792f8cf1c7cb88d
LeeD <lee.d.881010@gmail.com>
2022-02-19 17:18:35 +0900

    [Search] Make SearchVC & bind SearchTextField

--- SearchBook/SearchBook/SceneDelegate.swift
+++ SearchBook/SearchBook/SceneDelegate.swift
@@ -16,7 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScence = scene as? UIWindowScene else { return }
+        self.sceneStartWithCoordinator(windowScence)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -50,3 +51,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+private extension SceneDelegate {
+    
+    func sceneStartWithCoordinator(_ windowScene: UIWindowScene) {
+        self.window = UIWindow(windowScene: windowScene)
+        let navigationController = BaseNavigationController(viewModel: BaseNavigationViewModel())
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+    }
+}
---
+++ SearchBook/SearchBook/Views/Search/SearchTextField.swift
@@ -0,0 +1,61 @@
+//
+//  SearchTextField.swift
+//  SearchBook
+//
+//  Created by USER on 2022/02/19.
+//
+
+import UIKit
+
+final class SearchTextField: UIView {
+    
+    
+    init(viewModel: SearchTextFieldViewModel) {
+        self.viewModel = viewModel
+        super.init(frame: .zero)
+        self.setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private let viewModel: SearchTextFieldViewModel
+    
+    private lazy var textField: UITextField = {
+        let textField: UITextField = .init()
+        textField.returnKeyType = .search
+        textField.delegate = self
+        textField.placeholder = "검색어를 입력하세요."
+        return textField
+    }()
+}
+
+private extension SearchTextField {
+    
+    func setupView() {
+        self.backgroundColor = .white
+        [self.textField].forEach {
+            self.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            self.textField.topAnchor.constraint(equalTo: self.topAnchor, constant: 5.0),
+            self.textField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20.0),
+            self.textField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20.0),
+            self.textField.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5.0),
+        ])
+        self.textField.becomeFirstResponder()
+    }
+    
+}
+
+extension SearchTextField: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.text.map { self.viewModel.action(.search($0)) }
+        return true
+    }
+    
+}
